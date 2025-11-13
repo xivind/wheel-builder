049a1c682ab40876d13a771f2eacb4f4e3a4409a
oivind.langeland <oivind.langeland@gmail.com>
2025-11-13 23:50:49 +0000

    More stubs

---
+++ logger.py
@@ -0,0 +1,13 @@
+# Configure logging following established patterns
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+logger = logging.getLogger(__name__)
+
+def setup_logging():
+    """Configure logging to output to stdout/stderr"""
+    return logger
